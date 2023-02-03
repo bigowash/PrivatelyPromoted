79baa99b87ca39e659054c88f225d6cba40cca2e
bigowash <72806245+bigowash@users.noreply.github.com>
2023-02-03 17:54:54 +0000

    Initial commit

---
+++ web-app/static/main.js
@@ -0,0 +1,44 @@
+import Ajax from "./ajax.js";
+
+// Helper functions - to ease coding
+function id(id) {
+    return document.getElementById(id);
+}
+
+function qsa(selector) {
+    return document.querySelectorAll(selector);
+}
+
+function singleOption() {
+    // update stats
+    solving.push("singleOption()");
+    console.log(++clicks);
+
+    // prepare request
+    const request = {
+        "task": "single_option",
+        "options": options
+    };
+
+    const template = Ajax.query(request);
+    console.log("Request: " + JSON.stringify(request));
+
+    template.then(function (object) {
+        console.log("Response: " + JSON.stringify(object));
+
+        // if the function actually changed something, change the onscreen board
+        if (JSON.stringify(options) != JSON.stringify(object.options)) {
+            options = object.options;
+            update_board(false);
+        }
+    });
+}
+
+// Get the button elements
+const option_1 = id("option-1");
+
+// Equivalent of .onclick = function ()
+// (idk why that doesnt seem to work for me but this does)
+window.onload = function () {
+    option_1.addEventListener("click", singleOption);
+}
---
+++ web-app/static/users.js
@@ -0,0 +1,33 @@
+const users = [
+    {
+        id: 1,
+        name: "back-end-dev",
+        email: "administrator@gmail.com",
+        password: "1234",
+        type: "admin"
+    },
+    {
+        id: 2,
+        name: "facebook",
+        email: "facebook@gmail.com",
+        password: "1234",
+        type: "website"
+    },
+    {
+        id: 3,
+        name: "john",
+        email: "john@gmail.com",
+        password: "1234",
+        type: "user"
+    },
+    {
+        id: 4,
+        name: "googleAds",
+        email: "google@gmail.com",
+        password: "1234",
+        type: "advertiser"
+    },
+
+];
+
+module.exports = users;
