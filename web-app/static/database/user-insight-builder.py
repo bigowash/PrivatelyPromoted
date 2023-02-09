ad49b7a502aa8791400488163d9c76fc9cd4ee4d
bigowash <bui.theo@gmail.com>
2023-02-09 15:47:26 +0000

    completed until the user-save button

--- web-app/static/database/user-insight-builder.py
+++ web-app/static/database/user-insight-builder.py
@@ -11,12 +11,15 @@ import math
 keys = ['Age', 'Gender', 'Location', 'Interests', 'Income', 'Education', 'Occupation', 'Marital status', 'Family size', 'Home ownership', 'Language', 'Ethnicity', 'Religion', 'Political affiliation', 'Purchasing behavior', 'Online behavior', 'Mobile Usage Behaviors']
 keys_multiple = ['Interests', 'Language', 'Purchasing behavior', 'Online behavior', 'Mobile Usage Behaviors']
 
+insight_gen = [    "Amazon",    "Netflix",    "Spotify",    "YouTube",    "Facebook",    "Google",    "Instagram",    "TikTok",    "Pinterest",    "Twitter", "Experian",    "Equifax",    "TransUnion",    "Acxiom",    "Epsilon",    "Oracle Data Cloud",    "Neustar",    "Datalogix",    "LiveRamp",    "Epsilon"]
+
 # # Opening JSON file
 with open('./web-app/static/database/preferences.json') as json_file:
     data = json.load(json_file)
     ke = list(data.keys())
 
     for i in range(1, 5):
+        print(i)
         # create a user profile in json file
 
         # add some preferences
@@ -33,24 +36,45 @@ with open('./web-app/static/database/preferences.json') as json_file:
                 l = len(data[j])/2
                 l = math.floor(l)
 
+                y = {}
+
                 x = random.choices(data[j], k=random.randint(1, l))
                 x = list(dict.fromkeys(x))
-                temp[j] = x
+                y['values'] = x
+                y['certainty'] = []
+                y['selected'] = []
+                y['source'] = []
+                for u in range(len(x)):
+                    y['selected'].append(True)
+                    y['certainty'].append(random.randint(49, 100))
+                    y['source'].append(random.choice(insight_gen))
+                temp[j] = y
             else:
                 x = random.choice(data[j])
-                temp[j] = x
+                y = {}
+                y['selected'] = [True]
+                y['values'] = [x]
+                y['source'] = [random.choice(insight_gen)]
+                y['certainty'] = [random.randint(49, 100)]
+                temp[j] = y
         # print(temp)
 
         for j in ['Gender', 'Location', 'Age']:
             if j not in temp.keys():
                 # print(j, "not in")
-                y = random.choice(data[j])
-                # print(y)
-                # print(temp[j])
+                x = random.choice(data[j])
+
+                y = {}
+                y['selected'] = [True]
+                y['values'] = [x]
+                y['source'] = [random.choice(insight_gen)]
+                y['certainty'] = [random.randint(49, 100)]
+
                 temp[j] = y
     
 
         file_name = "user-"+str(i)
+        print(file_name)
 
         #erases current data
         open("./web-app/static/database/"+file_name+".json", 'w').close()
--- web-app/static/user-page.js
+++ web-app/static/user-page.js
@@ -1,8 +1,3 @@
-// const fs = require('fs');
-// const fs = module.constructor._load('fs');
-// import fs from './fs'
-// import * as fs from 'fs'
-
 // import Ajax from "./ajax.js";
 
 // Helper functions - to ease coding
@@ -13,6 +8,9 @@ function id(id) {
 function qsa(selector) {
     return document.querySelectorAll(selector);
 }
+
+// all the data here
+let userdata;
 
 // Get the information from the link (what user is selected)
 let params = new URLSearchParams(window.location.search);
@@ -30,48 +28,198 @@ async function addPreferences() {
     const response = await fetch('../database/' + user_filename)
         .then(response => response.text())
         .then(data => {
-            // console.log(data)
-
+            // data from file
             data = JSON.parse(data)
-            // console.log(data['Age']);
+            userdata = data;
 
-            console.log(Object.entries())
+            // get insights categories and sort alphabetically
+            const keys = [];
+            for (const i in data) {
+                keys.push(i)
+            }
+            keys.sort()
+
+            console.log("building the table");
+
+            // build the table
+
+            // Create the header row
+            const headerRow = document.createElement('tr');
+            const headerCheck = document.createElement('th');
+            headerCheck.textContent = 'Include';
+            headerRow.appendChild(headerCheck);
+            const headerCategory = document.createElement('th');
+            headerCategory.textContent = 'Category';
+            headerRow.appendChild(headerCategory);
+            const headerValue = document.createElement('th');
+            headerValue.textContent = 'Value';
+            headerRow.appendChild(headerValue);
+            const headerCertainty = document.createElement('th');
+            headerCertainty.textContent = 'Certainty';
+            headerRow.appendChild(headerCertainty);
+            const headerInput = document.createElement('th');
+            headerInput.textContent = 'Change';
+            headerRow.appendChild(headerInput);
+            const headerValidateInput = document.createElement('th');
+            headerValidateInput.textContent = 'Submit';
+            headerRow.appendChild(headerValidateInput);
+
+            table.appendChild(headerRow);
+
+            // create the body rows based of file info
+            keys.forEach(category => {
+                // console.log(Array.isArray(data[category]['values']))
+
+                if (Array.isArray(data[category]['values'])) {
+                    for (let i = 0; i < data[category]['values'].length; i++) {
+                        const tempInfo = {
+                            "category": category,
+                            'values': data[category]['values'][i],
+                            'certainty': data[category]['certainty'][i],
+                            'selected': data[category]['selected'][i],
+                            'source': data[category]['source'][i]
+                        }
+                        const dataRow = makeTable(tempInfo, i)
+                        table.appendChild(dataRow);
+                    }
+                } else {
+                    const tempInfo = {
+                        "category": category,
+                        'values': data[category]['values'],
+                        'certainty': data[category]['certainty'],
+                        'selected': data[category]['selected'],
+                        'source': data[category]['source']
+                    }
+                    const dataRow = makeTable(tempInfo, 1)
+                    table.appendChild(dataRow);
+                }
+
+            });
+
+
 
         })
 }
 
-console.log("hese")
+function makeTable(values, i) {
+    const dataRow = document.createElement('tr');
+
+    // check
+    const inputSection = document.createElement('td');
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.checked = values['selected'];
+    input.setAttribute('name', values["category"] + i);
+    input.setAttribute('id', values["category"] + i + 'check');
+    inputSection.appendChild(input);
+    dataRow.appendChild(inputSection);
+
+    // category
+    // console.log("adding cat", values["category"])
+    const catSection = document.createElement('td');
+    catSection.textContent = values["category"];
+    dataRow.appendChild(catSection);
+
+    // value
+    const valueSection = document.createElement('td');
+    valueSection.textContent = values["values"];
+    // console.log(valueSection.textContent)
+    dataRow.appendChild(valueSection);
+
+    // certainty
+    const certaintySection = document.createElement('td');
+    certaintySection.textContent = values["certainty"];
+    dataRow.appendChild(certaintySection);
+
+    // source
+    const sourceSection = document.createElement('td');
+    sourceSection.textContent = values["source"];
+    dataRow.appendChild(sourceSection);
+
+    // change 
+    const changeSection = document.createElement('td');
+    const changeText = document.createElement('input');
+    changeText.setAttribute('type', 'text');
+    changeText.setAttribute('name', values["category"]);
+    changeText.setAttribute('id', values["category"] + i + ' change');
+    // changeText.setAttribute('id', values["category"] + '_id');
+    changeSection.appendChild(changeText);
+    dataRow.appendChild(changeSection);
+
+    // submit button 
+    const submitSection = document.createElement('td');
+    const submitButton = document.createElement('button');
+    submitButton.classList += "submit-button"
+    submitButton.setAttribute('id', values["category"] + i + ' button');
+    submitButton.addEventListener('click', function () {
+        changeValue(values["category"] + i);
+    });
+    submitSection.appendChild(submitButton);
+    dataRow.appendChild(submitSection);
+
+
+    return dataRow;
+}
+
 addPreferences();
 
-
 const saveButton = document.getElementById("save");
+const selectAllButton = document.getElementById("selectAll");
+const deselectAllButton = document.getElementById("deselectAll");
 
 saveButton.addEventListener("click", function () {
     const selectedCategories = {};
     const checkboxes = qsa('input[type="checkbox"]');
 
     checkboxes.forEach(function (checkbox) {
+        // console.log("checkboc", checkbox.name);
+        const category = checkbox.name.split(/(\d+)/)
         if (checkbox.checked) {
-            selectedCategories[checkbox.name] = true;
+            userdata[category[0]][category[1]] = true;
+        } else {
+            userdata[category[0]][category[1]] = false;
         }
     });
 
-    console.log("here");
-    console.log(selectedCategories);
-
-    const json = JSON.stringify(selectedCategories);
-    const blob = new Blob([json], { type: "application/json" });
-    const href = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = href;
-    link.download = "selected_categories.json";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    writeToFile(userdata);
 });
 
+selectAllButton.addEventListener("click", function () {
+    console.log("selecting all");
+    const checkboxes = qsa('input[type="checkbox"]');
+    checkboxes.forEach(function (checkbox) {
+        checkbox.checked = true;
+    });
+});
 
+deselectAllButton.addEventListener("click", function () {
+    console.log("deselecting all");
+    const checkboxes = qsa('input[type="checkbox"]');
+    checkboxes.forEach(function (checkbox) {
+        checkbox.checked = false;
+    });
+});
 
+function changeValue(category) {
+
+}
+
+async function writeToFile(data) {
+    console.log(data);
+    const response = await fetch('./database/' + user_filename,
+        {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(response => console.log(response.json()))
+        .then(data => {
+            console.log("successfully writtent o file")
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    console.log("made it rhur")
+}
 // using the user id information. need to build out the profile of the person.
 // need a boiler plate user dashboard page. what information to include, and then
 // chagne that dynamically using the userid. and the associaed data file.(user-1)
