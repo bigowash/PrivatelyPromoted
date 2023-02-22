7a9e623d0636c61ceb79ae1a925561720d7d02c8
bigowash <bui.theo@gmail.com>
2023-02-22 16:16:18 +0000

    tryingout something new

--- web-app/static/ajax.js
+++ web-app/static/ajax.js
@@ -2,40 +2,40 @@ const Ajax = Object.create(null);
 
 const json = (response) => response.json();
 
-Ajax.query = function (requestObj) {
-    return window
-        .fetch("/", {
-            method: "POST",
-            body: JSON.stringify(requestObj),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-        .then(json);
-};
-
 // Ajax.query = function (requestObj) {
-//     return new Promise(function (resolve, reject) {
-//         window
-//             .fetch("/", {
-//                 method: "POST",
-//                 body: JSON.stringify(requestObj),
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                 },
-//             })
-//             .then(function (response) {
-//                 if (response.ok) {
-//                     resolve(response.json());
-//                 } else {
-//                     reject(new Error('Ajax query failed with status ' + response.status));
-//                 }
-//             })
-//             .catch(function (error) {
-//                 reject(error);
-//             });
-//     });
+//     return window
+//         .fetch("/", {
+//             method: "POST",
+//             body: JSON.stringify(requestObj),
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//         })
+//         .then(json);
 // };
+
+Ajax.query = function (requestObj) {
+    return new Promise(function (resolve, reject) {
+        window
+            .fetch("/", {
+                method: "POST",
+                body: JSON.stringify(requestObj),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(function (response) {
+                if (response.ok) {
+                    resolve(response.json());
+                } else {
+                    reject(new Error('Ajax query failed with status ' + response.status));
+                }
+            })
+            .catch(function (error) {
+                reject(error);
+            });
+    });
+};
 
 
 export default Object.freeze(Ajax);
