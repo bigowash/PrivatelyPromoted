11a4c0d657f8626cc0e7644f30f423f49ca7fcdc
Max <mlm20@ic.ac.uk>
2023-02-24 18:27:59 +0000

    Changes made to user page styling

--- web-app/static/user-page/user-page.js
+++ web-app/static/user-page/user-page.js
@@ -1,13 +1,13 @@
 import Ajax from "../ajax.js";
 
 // Helper functions - to ease coding
-function id(id) {
+const id = function (id) {
     return document.getElementById(id);
-}
+};
 
-function qsa(selector) {
+const qsa = function (selector) {
     return document.querySelectorAll(selector);
-}
+};
 
 // all the data here
 let userdata;
@@ -18,23 +18,37 @@ const sourcesList = [];
 // Get the information from the link (what user is selected)
 const params = new URLSearchParams(window.location.search);
 const userid = params.get("id");
+const userName = params.get("name");
 const user_filename = "user-" + userid + ".json";
 console.log("id: ", userid);
 console.log("filename: ", user_filename);
 
-// Make the preferences table from the user file
+// Fill in subtitle
+const subtitle = id("subtitle");
+subtitle.textContent = `Welcome to Settings ${userName}, here you can adjust the insights related to your data profile.`;
+
+// Add homepage link to title
+const title = id("title");
+title.addEventListener("click", function () {
+    window.location.href = "../";
+});
+
+///
+// Table creation
+///
+
 const table = id("preferences-table");
-async function addPreferences() {
+const addPreferences = async function () {
     console.log("Preferences are loading");
 
     const response = await fetch("../database/userfiles/" + user_filename)
         .then((response) => response.text())
         .then((data) => {
-            // data from file
+            // Parse JSON data and save to variable
             data = JSON.parse(data);
             userdata = data;
 
-            // get insights categories and sort alphabetically
+            // Get insights categories and sort alphabetically
             const keys = [];
             for (const i in data) {
                 keys.push(i);
@@ -43,30 +57,45 @@ async function addPreferences() {
 
             console.log("building the table");
 
-            // build the table
+            /// Build the table
 
             // Create the header row
-            const headerRow = document.createElement("tr");
+            const headerRow = document.createElement("thead");
+            headerRow.classList.add("headerRow");
+
             const headerCheck = document.createElement("th");
             headerCheck.textContent = "Include";
+            headerCheck.classList.add("headerCell");
             headerRow.appendChild(headerCheck);
+
             const headerCategory = document.createElement("th");
             headerCategory.textContent = "Category";
+            headerCategory.classList.add("headerCell");
             headerRow.appendChild(headerCategory);
+
             const headerValue = document.createElement("th");
             headerValue.textContent = "Value";
+            headerValue.classList.add("headerCell");
             headerRow.appendChild(headerValue);
+
             const headerCertainty = document.createElement("th");
             headerCertainty.textContent = "Certainty";
+            headerCertainty.classList.add("headerCell");
             headerRow.appendChild(headerCertainty);
+
             const headerSource = document.createElement("th");
             headerSource.textContent = "Source";
+            headerSource.classList.add("headerCell");
             headerRow.appendChild(headerSource);
+
             const headerInput = document.createElement("th");
             headerInput.textContent = "Change";
+            headerInput.classList.add("headerCell");
             headerRow.appendChild(headerInput);
+
             const headerValidateInput = document.createElement("th");
             headerValidateInput.textContent = "Submit";
+            headerValidateInput.classList.add("headerCell");
             headerRow.appendChild(headerValidateInput);
 
             table.appendChild(headerRow);
@@ -110,42 +139,47 @@ async function addPreferences() {
                     }
                 }
             });
-            // addSelector();
         });
-}
+};
 
-function makeTable(values, i, insightProvider) {
+const makeTable = function (values, i, insightProvider) {
     const dataRow = document.createElement("tr");
+    dataRow.classList.add("tableRow");
 
-    // check
+    /// check
     const inputSection = document.createElement("td");
     const input = document.createElement("input");
     input.setAttribute("type", "checkbox");
     input.checked = values.selected;
     input.setAttribute("name", insightProvider + values.category + i);
-    input.setAttribute(
-        "id",
-        insightProvider + values.category + i + "check"
-    );
+    input.setAttribute("id", insightProvider + values.category + i + "check");
+
+    inputSection.classList.add("checkbox");
+    inputSection.classList.add("tableBodyCell");
     inputSection.appendChild(input);
     dataRow.appendChild(inputSection);
 
-    // category
+    /// category
     // console.log("adding cat", values["category"])
     const catSection = document.createElement("td");
     catSection.textContent = values.category;
+    catSection.classList.add("category");
+    catSection.classList.add("tableBodyCell");
     dataRow.appendChild(catSection);
 
     // value
     const valueSection = document.createElement("td");
     valueSection.textContent = values.values;
     valueSection.id = insightProvider + values.category + i + "value";
-    // console.log(valueSection.textContent)
+    valueSection.classList.add("valueCell");
+    valueSection.classList.add("tableBodyCell");
     dataRow.appendChild(valueSection);
 
     // certainty
     const certaintySection = document.createElement("td");
     certaintySection.textContent = values.certainty;
+    certaintySection.classList.add("certaintyCell");
+    certaintySection.classList.add("tableBodyCell");
     dataRow.appendChild(certaintySection);
 
     // source + add to the sources list
@@ -154,6 +188,8 @@ function makeTable(values, i, insightProvider) {
     }
     const sourceSection = document.createElement("td");
     sourceSection.textContent = values.source;
+    sourceSection.classList.add("sourceCell");
+    sourceSection.classList.add("tableBodyCell");
     dataRow.appendChild(sourceSection);
 
     // change
@@ -165,33 +201,33 @@ function makeTable(values, i, insightProvider) {
         "id",
         insightProvider + values.category + i + "change"
     );
-    // changeText.setAttribute('id', values["category"] + '_id');
+    changeSection.classList.add("changeTextCell");
+    changeSection.classList.add("tableBodyCell");
     changeSection.appendChild(changeText);
     dataRow.appendChild(changeSection);
 
     // submit button
     const submitSection = document.createElement("td");
     const submitButton = document.createElement("button");
-    submitButton.classList += "submit-button";
+    submitSection.classList.add("submit-button");
+    submitSection.classList.add("tableBodyCell");
     submitButton.setAttribute(
         "id",
         insightProvider + values.category + i + "button"
     );
     submitButton.addEventListener("click", function () {
-        changeValue(
-            insightProvider + values.category + i,
-            values.category
-        );
+        changeValue(insightProvider + values.category + i, values.category);
     });
     submitSection.appendChild(submitButton);
     dataRow.appendChild(submitSection);
 
     return dataRow;
-}
+};
+
 const tableCat = id("categories");
 console.log(tableCat);
 
-function addSelector() {
+const addSelector = function () {
     // console.log("in th sec");
     // categories first
     // console.log(categoriesList)
@@ -240,7 +276,7 @@ function addSelector() {
         row.appendChild(col2);
         tableCat.appendChild(row);
     }
-}
+};
 
 addPreferences();
 
@@ -250,7 +286,7 @@ const deselectAllButton = document.getElementById("deselectAll");
 
 saveButton.addEventListener("click", function () {
     const selectedCategories = {};
-    const checkboxes = qsa("input[type=\"checkbox\"]");
+    const checkboxes = qsa('input[type="checkbox"]');
 
     checkboxes.forEach(function (checkbox) {
         // console.log("checkboc", checkbox.name);
@@ -268,7 +304,7 @@ saveButton.addEventListener("click", function () {
 
 selectAllButton.addEventListener("click", function () {
     console.log("selecting all");
-    const checkboxes = qsa("input[type=\"checkbox\"]");
+    const checkboxes = qsa('input[type="checkbox"]');
     checkboxes.forEach(function (checkbox) {
         checkbox.checked = true;
     });
@@ -276,7 +312,7 @@ selectAllButton.addEventListener("click", function () {
 
 deselectAllButton.addEventListener("click", function () {
     console.log("deselecting all");
-    const checkboxes = qsa("input[type=\"checkbox\"]");
+    const checkboxes = qsa('input[type="checkbox"]');
     checkboxes.forEach(function (checkbox) {
         checkbox.checked = false;
     });
@@ -305,7 +341,7 @@ function changeValue(idFinder, category) {
     }
 }
 
-async function writeToFile(data, filename) {
+const writeToFile = async function (data, filename) {
     console.log(data);
 
     // prepare request
@@ -324,33 +360,4 @@ async function writeToFile(data, filename) {
 
         // rest of the code here
     });
-}
-
-// function selectChange(v, el) {
-//     if (v == "sources") {
-
-//     } else if (v == "category") {
-
-//     }
-// }
-// using the user id information. need to build out the profile of the person.
-// need a boiler plate user dashboard page. what information to include, and then
-// chagne that dynamically using the userid. and the associaed data file.(user-1)
-
-/** Dashborad
- * Incoming insights to carying degrees of speficicity
- *      how specific
- *      there should be more questions here
- * Filter
- * Prvacy selector settings
- * How are the different insights filtered? grouped together?
- * Blocking insihgts from certain companies?
- * Crypto area. Maybe do a tabbed kindof thing where I can see the crypto information?
- *      Or maybe more specific in the tab, less specific in the dashboard - yes
- * typical ads?
- *      an example of an ad that advertisers have selected to also display the ad
- *      selection process.
- *
- *
- */
-
+};
