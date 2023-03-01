778eab30405804891892836e2a9e1766760f4735
Max <mlm20@ic.ac.uk>
2023-03-01 18:09:11 +0000

    started work on insights page

--- web-app/static/insight-page/insight-page.js
+++ web-app/static/insight-page/insight-page.js
@@ -18,7 +18,7 @@ const usesText = document.getElementById("uses_total");
 const userButtons = document.getElementById("userButtons");
 const table = document.getElementById("table");
 
-async function addPreferences() {
+const addPreferences = async function () {
     console.log("Preferences are loading");
 
     // delete current buttons
@@ -28,7 +28,7 @@ async function addPreferences() {
 
     // prepare request
     const request = {
-        task: "get-user-buttons",
+        task: "get-user-buttons"
     };
 
     const template = Ajax.query(request);
@@ -38,7 +38,7 @@ async function addPreferences() {
     template.then(function (object) {
         console.log("Response: " + JSON.stringify(object));
         // rest of the code here
-        const data = object.data
+        const data = object.data;
         // console.log(data)
 
         const lines = data.split("\n");
@@ -67,38 +67,7 @@ async function addPreferences() {
             userButtons.appendChild(a);
         }
     });
-
-
-    // const response = await fetch("../database/userProfiles.csv")
-    //     .then(response => response.text())
-    //     .then(data => {
-    //         const lines = data.split("\n");
-
-    //         for (let i = 1; i < lines.length - 1; i++) {
-    //             const el = lines[i].split(", ");
-    //             const name = el[1];
-    //             let id = el[2];
-    //             // console.log(id);
-
-    //             id = id.replace(/\r/g, "");
-    //             // profile_ids.push(id)
-    //             // console.log("el", el);
-
-    //             const a = document.createElement("button");
-    //             a.classList += "intro-button";
-    //             // let link = "../img/" + img_file + "/" + id + ".jpeg"
-    //             // a.innerHTML = '<img src = ' + link + ' alt = ' + name + '>'
-    //             a.textContent = id;
-    //             a.id += "user-" + id;
-
-    //             a.addEventListener("click", function () {
-    //                 profileSelected(id);
-    //             });
-    //             // console.log("in here");
-    //             userButtons.appendChild(a);
-    //         }
-    //     });
-}
+};
 
 button.addEventListener("click", async function () {
     console.log("Button Clicked");
@@ -116,18 +85,11 @@ button.addEventListener("click", async function () {
     console.log("Request: " + JSON.stringify(request));
     const template = await Ajax.query(request);
 
-    // // upon the return of the request
-    // template.then(function (object) {
-    //     console.log("Response: " + JSON.stringify(object));
-    //     // rest of the code here
-    //     addPreferences();
-    // });
     console.log("Response: " + JSON.stringify(template));
-    addPreferences()
-
+    addPreferences();
 });
 
-async function profileSelected(id) {
+const profileSelected = async function (id) {
     console.log("Clear our the table");
     while (table.firstChild) {
         table.removeChild(table.firstChild);
@@ -145,7 +107,7 @@ async function profileSelected(id) {
     // upon the return of the request
     template.then(function (object) {
         console.log("Response: " + JSON.stringify(object));
-        let data = object.data
+        let data = object.data;
         // data from file
         data = JSON.parse(data);
 
@@ -183,7 +145,7 @@ async function profileSelected(id) {
         // console.log("element");
 
         // create the body rows based of file info
-        keys.forEach(category => {
+        keys.forEach((category) => {
             // console.log(data[category].length)
 
             // console.log(Array.isArray(data[category]['values']))
@@ -230,99 +192,10 @@ async function profileSelected(id) {
         });
         moneyText.textContent = totalMoney;
         usesText.textContent = totalUses;
-    })
+    });
+};
 
-    // const response = await fetch("../database/userfiles/user-" + id + ".json")
-    //     .then(response => response.text())
-    //     .then(data => {
-    //         // data from file
-    //         data = JSON.parse(data);
-
-    //         // get insights categories and sort alphabetically
-    //         const keys = [];
-    //         for (const i in data) {
-    //             keys.push(i);
-    //         }
-    //         keys.sort();
-
-    //         console.log("building the table");
-    //         // console.log("data", data)
-
-    //         // build the table
-
-    //         // Create the header row
-    //         const headerRow = document.createElement("tr");
-    //         const headerCategory = document.createElement("th");
-    //         headerCategory.textContent = "Category";
-    //         headerRow.appendChild(headerCategory);
-    //         const headerValue = document.createElement("th");
-    //         headerValue.textContent = "Value";
-    //         headerRow.appendChild(headerValue);
-    //         const headerCertainty = document.createElement("th");
-    //         headerCertainty.textContent = "Certainty";
-    //         headerRow.appendChild(headerCertainty);
-    //         const headerMoney = document.createElement("th");
-    //         headerMoney.textContent = "MoneyIn";
-    //         headerRow.appendChild(headerMoney);
-    //         const headerTimes = document.createElement("th");
-    //         headerTimes.textContent = "#ofTimesUsed";
-    //         headerRow.appendChild(headerTimes);
-
-    //         table.appendChild(headerRow);
-    //         // console.log("element");
-
-    //         // create the body rows based of file info
-    //         keys.forEach(category => {
-    //             // console.log(data[category].length)
-
-    //             // console.log(Array.isArray(data[category]['values']))
-    //             for (let index = 0; index < data[category].length; index++) {
-    //                 const element = data[category][index];
-    //                 // console.log("element");
-    //                 // console.log(element);
-    //                 // console.log(element["source"]);
-    //                 // add to the list of categories
-
-    //                 if (Array.isArray(element.values)) {
-    //                     for (let i = 0; i < element.values.length; i++) {
-    //                         if (element.source[i] === name) {
-    //                             const tempInfo = {
-    //                                 category,
-    //                                 values: element.values[i],
-    //                                 certainty: element.certainty[i],
-    //                                 selected: element.selected[i],
-    //                                 source: element.source[i],
-    //                                 money: element.money[i],
-    //                                 times: element.times[i]
-    //                             };
-    //                             const dataRow = makeTable(tempInfo, i, index);
-    //                             table.appendChild(dataRow);
-    //                         }
-    //                     }
-    //                 } else {
-    //                     if (element.source == name) {
-    //                         const tempInfo = {
-    //                             category,
-    //                             values: element.values,
-    //                             certainty: element.certainty,
-    //                             selected: element.selected,
-    //                             source: element.source,
-    //                             money: element.money,
-    //                             times: element.times
-    //                         };
-    //                         // const dataRow = makeTable(tempInfo, 1)
-    //                         const dataRow = makeTable(tempInfo, 1, index);
-    //                         table.appendChild(dataRow);
-    //                     }
-    //                 }
-    //             }
-    //         });
-    //         moneyText.textContent = totalMoney;
-    //         usesText.textContent = totalUses;
-    //     });
-}
-
-function makeTable(values, i, insightProvider) {
+const makeTable = function (values, i, insightProvider) {
     // console.log(values, i, insightProvider)
     const dataRow = document.createElement("tr");
 
@@ -358,5 +231,6 @@ function makeTable(values, i, insightProvider) {
     dataRow.appendChild(timesSec);
 
     return dataRow;
-}
+};
+
 addPreferences();
