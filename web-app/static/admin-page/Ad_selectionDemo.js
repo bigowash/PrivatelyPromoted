76c5287ce261e9fad7c4c58a2033df597743fa26
Max <mlm20@ic.ac.uk>
2023-03-02 11:47:26 +0000

    updated card text on advert dashboard

--- web-app/static/admin-page/Ad_selectionDemo.js
+++ web-app/static/admin-page/Ad_selectionDemo.js
@@ -13,22 +13,21 @@ let total;
 const response = await fetch("../database/history.json")
     .then((response) => response.text())
     .then(function (data) {
-        data = JSON.parse(data)
-        total = data[ad_id]
-        console.log(total)
+        data = JSON.parse(data);
+        total = data[ad_id];
+        console.log(total);
 
         // fill up the parts
         // part 1 -- the total insights for the user
-        let box1 = document.getElementById("p1")
+        let box1 = document.getElementById("p1");
 
-        const headerNmae = document.createElement("h3")
-        headerNmae.textContent = total.username
-        box1.appendChild(headerNmae)
+        const headerNmae = document.createElement("h3");
+        headerNmae.textContent = total.username;
+        box1.appendChild(headerNmae);
 
-        const listEl = document.createElement("ul")
+        const listEl = document.createElement("ul");
 
         // console.log(total.username)
-
 
         for (const [key, value] of Object.entries(total.userdata)) {
             // Create a new list item element for each pair
@@ -45,12 +44,12 @@ const response = await fetch("../database/history.json")
 
             // Add the list item to the unordered list
         }
-        box1.appendChild(listEl)
+        box1.appendChild(listEl);
 
         // part 2 -- the selected insights
-        let box2 = document.getElementById("p2")
+        let box2 = document.getElementById("p2");
 
-        const listEl2 = document.createElement("ul")
+        const listEl2 = document.createElement("ul");
 
         for (const [key, value] of Object.entries(total.userdata)) {
             // Create a new list item element for each pair
@@ -58,7 +57,6 @@ const response = await fetch("../database/history.json")
             // Set the text content of the list item to the key:value pair
             // console.log("ehkfslfd")
             for (let i = 0; i < value[0].values.length; i++) {
-
                 const element = value[0].values[i];
 
                 if (!value[0].selected[i]) {
@@ -72,17 +70,17 @@ const response = await fetch("../database/history.json")
 
             // Add the list item to the unordered list
         }
-        box2.appendChild(listEl2)
+        box2.appendChild(listEl2);
 
         // part 3 -- the website data
-        let box3 = document.getElementById("p3")
-        console.log(total.website)
+        let box3 = document.getElementById("p3");
+        console.log(total.website);
 
         const website_name = document.createElement("h3");
         website_name.textContent = total.website.name;
-        box3.appendChild(website_name)
+        box3.appendChild(website_name);
 
-        const listEl3 = document.createElement("ul")
+        const listEl3 = document.createElement("ul");
 
         for (let i = 0; i < total.website.selectedThemes.length; i++) {
             const themese = total.website.selectedThemes[i];
@@ -93,20 +91,20 @@ const response = await fetch("../database/history.json")
         }
 
         // Add the list item to the unordered list
-        box3.appendChild(listEl3)
+        box3.appendChild(listEl3);
 
         // part 4 -- 3 advertismeents include the one selected
-        let box4 = document.getElementById("p4")
+        let box4 = document.getElementById("p4");
 
         // get 2 random advertisemnets
         const res = fetch("../database/adverts/data.json")
             .then((res) => res.text())
             .then(function (yda) {
-                yda = JSON.parse(yda)
+                yda = JSON.parse(yda);
 
                 // Get 2 random elements from the list
                 const randomElements = [];
-                randomElements.push(total.ad)
+                randomElements.push(total.ad);
                 while (randomElements.length < 3) {
                     const randomIndex = Math.floor(Math.random() * yda.length);
                     const randomElement = yda[randomIndex];
@@ -118,71 +116,71 @@ const response = await fetch("../database/history.json")
                 for (let i = 0; i < randomElements.length; i++) {
                     const addd = randomElements[i];
                     // for every add, add to the box relevant information. required pref and reccomended prefs.
-                    const tempReq = Object.entries(addd.requiredTargetedDemographic);
-                    const tempRec = Object.entries(addd.recommendedTargetedDemographic);
+                    const tempReq = Object.entries(
+                        addd.requiredTargetedDemographic
+                    );
+                    const tempRec = Object.entries(
+                        addd.recommendedTargetedDemographic
+                    );
 
-                    const ad_num = document.createElement("h3")
-                    ad_num.textContent = "Advertisement: " + (i + 1).toString()
-                    box4.appendChild(ad_num)
+                    const ad_num = document.createElement("h3");
+                    ad_num.textContent = "Advertisement: " + (i + 1).toString();
+                    box4.appendChild(ad_num);
 
-                    const head = document.createElement("h4")
-                    head.textContent = "Required Preferences"
-                    box4.appendChild(head)
+                    const head = document.createElement("h4");
+                    head.textContent = "Required Preferences";
+                    box4.appendChild(head);
 
-                    const ul1 = document.createElement("ul")
+                    const ul1 = document.createElement("ul");
                     for (let j = 0; j < tempReq.length; j++) {
                         const element = tempReq[j];
-                        const li = document.createElement("li")
-                        li.textContent = element[0] + ": " + element[1]
-                        ul1.appendChild(li)
+                        const li = document.createElement("li");
+                        li.textContent = element[0] + ": " + element[1];
+                        ul1.appendChild(li);
                     }
-                    box4.appendChild(ul1)
+                    box4.appendChild(ul1);
 
-                    const head3 = document.createElement("h4")
-                    head3.textContent = "Reccomended Preferences"
-                    box4.appendChild(head3)
+                    const head3 = document.createElement("h4");
+                    head3.textContent = "Reccomended Preferences";
+                    box4.appendChild(head3);
 
-                    const ul2 = document.createElement("ul")
+                    const ul2 = document.createElement("ul");
                     for (let j = 0; j < tempRec.length; j++) {
                         const element = tempRec[j];
-                        const li = document.createElement("li")
-                        li.textContent = element[0] + ": " + element[1]
-                        ul2.appendChild(li)
+                        const li = document.createElement("li");
+                        li.textContent = element[0] + ": " + element[1];
+                        ul2.appendChild(li);
                     }
-                    box4.appendChild(ul2)
-
+                    box4.appendChild(ul2);
                 }
 
                 // part 5 -- the final selection
-                let box5 = document.getElementById("p5")
+                let box5 = document.getElementById("p5");
 
-                const ss = document.createElement("h3")
-                ss.textContent = "Advertisement 1"
-                box5.appendChild(ss)
+                const ss = document.createElement("h3");
+                ss.textContent = "Advertisement 1";
+                box5.appendChild(ss);
 
-                const ul4 = document.createElement("ul")
+                const ul4 = document.createElement("ul");
                 for (const [key, value] of Object.entries(total.ad)) {
-                    const li32 = document.createElement("li")
-                    li32.textContent = `${key} : ${value}`
+                    const li32 = document.createElement("li");
+                    li32.textContent = `${key} : ${value}`;
 
-                    if (typeof value === 'object' && value !== null) {
-
-                        let text = ""
+                    if (typeof value === "object" && value !== null) {
+                        let text = "";
                         for (const [key1, val1] of Object.entries(value)) {
-                            text += `${key1} : ${val1} \n`
+                            text += `${key1} : ${val1} \n`;
                         }
-                        li32.textContent = `${key}:  ${text}`
+                        li32.textContent = `${key}:  ${text}`;
                     }
-                    ul4.appendChild(li32)
-
+                    ul4.appendChild(li32);
 
                     // Add the list item to the unordered list
                 }
-                box5.appendChild(ul4)
+                box5.appendChild(ul4);
                 const img = document.createElement("img");
-                img.id = "ad_image"
+                img.id = "ad_image";
                 img.src = "../database/adverts/" + total.ad.image;
-                box5.appendChild(img)
-            })
-    })
-
+                box5.appendChild(img);
+            });
+    });
